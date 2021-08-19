0775bdba221181b5afde14964888cf9463934b25
Eze Bernardine May <demayous1@gmail.com>
2021-08-19 10:46:26 +0100

    Created airtime and cashwithdrawal pages

---
+++ pages/cashWithdrawal.js
@@ -0,0 +1,10 @@
+import _CashWithdrawal from "../screens/CashWithdrawal";
+import Layout from "../components/Layout";
+
+export default function CashWithdrawal() {
+  return (
+    <Layout>
+      <_CashWithdrawal />
+    </Layout>
+  );
+}
