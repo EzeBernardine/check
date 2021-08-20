60210de70b315f0d393629d97f694ade4d6050dd
Eze Bernardine May <demayous1@gmail.com>
2021-08-20 13:59:20 +0100

    Deleted package.lock.json and ran yarn

--- screens/Landing/Cash/styles.js
+++ screens/Landing/Cash/styles.js
@@ -1,10 +1,8 @@
 import styled from "styled-components";
 import { Flex, Grid } from "../../../components/Box/styles";
 
-export const Container = styled.div`
+export const Container = styled.div``;
 
-`;
- 
 export const _Span = styled.span`
   color: white;
 `;
