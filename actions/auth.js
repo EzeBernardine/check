f0c507331b5ec257c1e95d24f639e6e50e80970c
Micheal Akinwonmi <michealakinwonmi@gmail.com>
2021-09-12 14:16:41 +0100

    Changed distDir

--- actions/auth.js
+++ actions/auth.js
@@ -7,7 +7,7 @@ import {getHeaders} from "../lib/utils";
 export const auth = async (baseURL) => {
     try {
         console.log( getHeaders(), ' getHeaders()')
-        const {data} = await axios.get(baseURL + `/auths/me`,{
+        const {data} = await axios.get(baseURL + `/users/v1/auths/me`,{
             headers: getHeaders()
         });
         return {
